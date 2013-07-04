948c479fe3c825ac3f68cb00570811dda18f44f6
Arman Ayvazyan <arman@ayvazyan.com>
2013-07-04 05:21:50 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+
+var data = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  
+});
+
+data.buffer.toString("utf-8");
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
