08636da25411e70a0084db666e2edbd4330327b1
Arman Ayva <arman@ayvazyan.com>
2013-07-30 19:11:45 -0400

    init

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
-
-var data = fs.readFileSync('index.html', function (err, data) {
+var fs = require('fs');
+var index = fs.readFileSync('index.html',"utf8",function (err, data) {
   if (err) throw err;
-  
+  console.log(data);
 });
 
-data.buffer.toString("utf-8");
+var buffer = new Buffer(index, "utf-8")
 
-
-var express = require('express');
+var  express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
